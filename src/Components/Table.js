f8daf3ddb00677b742c935bd9ffb74f3705a6874
Mehdi Rizvi <mehdirizvi03@gmail.com>
2021-02-18 17:59:53 -0500

    Added props for Table

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
-function Table(){
+function Table(props){
     return(
-        <div>
+        <div className="table">
+<table>
+    <tr>
+        <th> </th> {/*-this is a blank cell for where the images will be below */}
 
-            <h3 className="table">This is where the table/li goes</h3>
+        <th>Restaurant Name</th>
 
+        <th>Reviews</th>
+
+        <th>Distance from you</th>
+
+        <th>Price Range</th>
+    </tr>
+
+    <tr>
+        <td> <img src={props.imgUrl}/> </td>
+        <td><h3>{props.name}</h3>  </td>
+        <td><p>{props.review} </p></td>
+        <td><p>{props.distance}</p></td>
+        <td><p>{props.price}</p></td>
+    </tr>
+
+
+
+
+
+</table>
         </div>
     )
 }
